9736ae05d68481d73a37110062ac7114eb77f2f1
Ryan Volz <rvolz@mit.edu>
2020-04-21 21:11:04 -0400

    Try two ways at providing exe for python scripts on Windows for grc.
    Also add menu entry for Windows for grc.

--- recipe/install_grc.bat
+++ recipe/install_grc.bat
@@ -3,3 +3,7 @@ setlocal EnableDelayedExpansion
 cd build
 cmake -P grc/cmake_install.cmake
 if errorlevel 1 exit 1
+
+if not exist "%PREFIX%\Menu" mkdir "%PREFIX%\Menu"
+copy "%RECIPE_DIR%\menus\menu-grc-windows.json" "%PREFIX%\Menu"
+copy "grc\scripts\freedesktop\grc-icon-128.png" "%PREFIX%\Menu"
