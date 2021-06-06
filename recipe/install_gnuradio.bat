1d5b467a35ec6f68976700a0f52fe85e1e170e3c
Ryan Volz <ryan.volz@gmail.com>
2021-06-06 02:33:50 -0400

    Try working around implicit metapackage.

--- recipe/install_gnuradio.bat
+++
@@ -1,4 +0,0 @@
-:: script exists only as a workaround (see meta.yaml)
-:: it does not have to do anything
-echo "Packaging gnuradio metapackage."
-if errorlevel 1 exit 1
--- recipe/install_gnuradio.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# script exists only as a workaround (see meta.yaml)
-# it does not have to do anything
-echo "Packaging gnuradio metapackage."
