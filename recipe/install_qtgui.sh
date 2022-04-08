97ae13f33eba98fa1482d63ae0beb8f3f6ad46e3
Ryan Volz <ryan.volz@gmail.com>
2022-04-08 15:55:29 -0400

    Install the non-entry-point gr_filter_design.

--- recipe/install_qtgui.sh
+++ recipe/install_qtgui.sh
@@ -3,3 +3,5 @@
 cd build
 cmake -P gr-qtgui/cmake_install.cmake
 cmake -P gr-utils/plot_tools/cmake_install.cmake
+# install gr-filter apps to add previously-deleted gr_filter_design
+cmake -P gr-filter/cmake_install.cmake
