5e1dfd2bd1baeff50bf07054ff7841ffe2811f11
Ryan Volz <ryan.volz@gmail.com>
2022-04-08 13:07:39 -0400

    Move gr_filter_tool to qtgui package and add its dependencies.

--- recipe/install_qtgui.bat
+++ recipe/install_qtgui.bat
@@ -1,6 +1,11 @@
 setlocal EnableDelayedExpansion
 @echo on
 
+if not exist "%PREFIX%\Menu" mkdir "%PREFIX%\Menu"
+copy "%RECIPE_DIR%\menus\menu-gr_filter_design-windows.json" "%PREFIX%\Menu"
+
 cd build
 cmake -P gr-qtgui/cmake_install.cmake
+if errorlevel 1 exit 1
+cmake -P gr-utils/plot_tools/cmake_install.cmake
 if errorlevel 1 exit 1
