7a43718cc64c16d8db00b7ae7cc2f3a12273630c
Ryan Volz <ryan.volz@gmail.com>
2024-05-29 17:47:14 -0400

    Update Windows menu shortcuts to menuinst 2 format

--- recipe/install_qtgui.bat
+++ recipe/install_qtgui.bat
@@ -2,7 +2,7 @@ setlocal EnableDelayedExpansion
 @echo on
 
 if not exist "%PREFIX%\Menu" mkdir "%PREFIX%\Menu"
-copy "%RECIPE_DIR%\menus\menu-gr_filter_design-windows.json" "%PREFIX%\Menu"
+copy "%RECIPE_DIR%\menus\gnuradio-qtgui.json" "%PREFIX%\Menu"
 
 cd build
 cmake -P gr-qtgui/cmake_install.cmake
